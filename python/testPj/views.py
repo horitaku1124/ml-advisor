764a5fd99e08eb518f6f19904e75fb2ff14e96de
Horimatsu Takuya <new_woory@hotmail.com>
2021-09-14 00:43:22 +0900

    add: janome project

---
+++ python/testPj/views.py
@@ -0,0 +1,21 @@
+import json
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from janome.tokenizer import Tokenizer
+tokenizer = Tokenizer()
+
+@csrf_exempt
+def index(request):
+
+    if request.method == 'GET':
+        return JsonResponse({})
+
+    input = json.loads(request.body)
+    word = input["word"]
+
+    tokens = []
+    for token in tokenizer.tokenize(word, wakati=True):
+        tokens.append(token)
+
+    return JsonResponse({"tokens": tokens})
